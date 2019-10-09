0850fb04a712def0d1fd7752fdae6b7c0e25a5e4
GuillaumeZuff <guillaume.zufferey@gmail.com>
2019-10-09 12:28:21 +0200

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
                 background_color: `#ffffff`,
                 theme_color: `#ffffff`,
                 display: `minimal-ui`,
-                icon: `src/images/simpliquity_logo.png` // This path is relative to the root of the site.
+                icon: `src/images/simpliquity_logo_transparent.png` // This path is relative to the root of the site.
             }
         }
         // this (optional) plugin enables Progressive Web App + Offline functionality
